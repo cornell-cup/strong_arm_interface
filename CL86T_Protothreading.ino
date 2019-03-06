f7876e0f1eba1a143c0873f5bd8f023a5a0cb915
Rohan Agarwal <rohanag1@users.noreply.github.com>
2019-03-06 18:20:14 -0500

    Added some documentation

--- CL86T_Protothreading.ino
+++ CL86T_Protothreading.ino
@@ -1,19 +1,29 @@
+/* Variable Initializations*/
+
+//Does nothing
 const int enaneg = 22;
+
+//Goes to DIR- and PUL- on CL86T Stepper Driver
 const int dirneg = 24;
 const int pulneg = 26;
 
+//Goes to EA+ and EB+ on CL86T Stepper Driver
 const int encoderA = 18;
 const int encoderB = 19;
 
+//Counts the number of times the encoder pulses for a 200 step rotation
 int counter = 0;
+
+//
 unsigned long currentMillis = 0;
 unsigned long previousMillis = 0;
 unsigned long runMillis = 0;
+
 int stepInterval = 500;
 boolean stepPosition = false;
 volatile byte stateA = LOW;
 volatile byte stateB = LOW;
-int noStep = 200;
+int numStep = 200;
 double encoderTimeA[200];
 double encoderTimeB[200];
 int count = 0;
@@ -22,23 +32,30 @@ double totalTime = 0;
 int encoderCount = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  //Set "Control Signal" variables as output
   pinMode(enaneg,OUTPUT);
   pinMode(dirneg,OUTPUT);
   pinMode(pulneg,OUTPUT);
 
+  //Set "Encoder" signals as input
   pinMode(encoderA,INPUT);
   pinMode(encoderB,INPUT);
+  
+  //Set up interrupts on both "Encoder" signals to detect rising edge changes
   attachInterrupt(digitalPinToInterrupt(encoderA), senseEncoderA, RISING);
   attachInterrupt(digitalPinToInterrupt(encoderB), senseEncoderB, RISING);
+  
+  //Start the serial port
   Serial.begin(9600);
 }
 
+//ISR for Encoder A that records time of rising edge from start of program and increases pulse count (for 200 step rotation)
 void senseEncoderA(){
   encoderTimeA[counter] = currentMillis - runMillis;
   counter++; 
 }
 
+//ISR for Encoder B that records time of rising edge from start of program (pulse count increased by senseEncoderA -- both in line, just phase shifted)
 void senseEncoderB(){
   encoderTimeB[counter] = currentMillis - runMillis;
 }
@@ -48,7 +65,7 @@ void loop() {
   int x = 0;
   counter = 0;
   runMillis = micros();
-  while(x < noStep){
+  while(x < numStep){
     currentMillis = micros();
 //    encoderAPos = digitalRead(encoderA);
 //    encoderBPos = digitalRead(encoderB);
